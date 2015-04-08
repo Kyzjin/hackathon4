af8bfd38670ed1a4afabbedf869d74dedd6dfbf2
Kyzjin <jparish@vandals.uidaho.edu>
2015-04-08 15:56:23 -0700

    Update app.js

--- Resources/app.js
+++ Resources/app.js
@@ -1,3 +1,4 @@
+
 // this sets the background color of the master UIView (when there are no windows/tab groups on it)
 Titanium.UI.setBackgroundColor('#000');
 
@@ -10,16 +11,17 @@ var tabGroup = Titanium.UI.createTabGroup();
 //
 var win1 = Titanium.UI.createWindow({  
     title:'Friday, April 17th',
-    backgroundImage:"pictures/banner.jpg",
-    scrollable:true
+    backgroundImage:"pictures/banner.jpg"
 });
 var tab1 = Titanium.UI.createTab({  
     icon:'KS_nav_ui.png',
     title:'Friday Events',
     window:win1
 });
+
 var label1 = Titanium.UI.createLabel({
 	color:'black',
+	top:'37%',
 	text:'April 17th-18th',
 	font:{fontSize:60,fontFamily:'Helvetica Neue'},
 	textAlign:'center',
@@ -29,7 +31,8 @@ var label1 = Titanium.UI.createLabel({
 
 var ring = Titanium.UI.createButton({
 	title:'Official UI Class Ring Sales',
-	top: 100
+	top:'40%',
+	left:'20%'
 });
 ring.addEventListener('click', function(e) {
 	alert('Time - 1pm to 5pm\nPlace - VandaStore Plaza');
@@ -37,7 +40,8 @@ ring.addEventListener('click', function(e) {
 
 var race = Titanium.UI.createButton({
 	title:'Amazing Race Traditions Challenge',
-	top: 115
+	top:'40%',
+	left:'50%'
 });
 race.addEventListener('click', function(e) {
 	alert('Time - 5pm to 6pm\nPlace - Meet at VandalStore\nCost - $10, includes Tradition Keepers book');
@@ -45,7 +49,8 @@ race.addEventListener('click', function(e) {
 
 var gala = Titanium.UI.createButton({
 	title:'CALS Moms Weekend Gala Wine and Cheese Tasting',
-	top: 130
+	top:'50%',
+	left:'20%'
 });
 gala.addEventListener('click', function(e) {
 	alert('Time - 5pm to 8pm\nPlace - Pitman Center International Ballroom\nCost - $20/person or $35/couple\nRSVP Required');
@@ -53,7 +58,8 @@ gala.addEventListener('click', function(e) {
 
 var bella = Titanium.UI.createButton({
 	title:'Bella Notte: An Italian Dinner',
-	top: 145
+	top:'50%',
+	left:'50%'
 });
 bella.addEventListener('click', function(e) {
 	alert('Time - 5pm to 8:30pm\nPlace - St. Augustines Catholic Center\nCost - $5/student, $6/person, $18/group of 4');
@@ -82,13 +88,14 @@ var tab2 = Titanium.UI.createTab({
 
 var label2 = Titanium.UI.createLabel({
 	color:'black',
+	top:'30%',
 	text:'Welcome Moms',
 	font:{fontSize:35,fontFamily:'Helvetica Neue'},
-	textAlign:'top',
-	width:'auto'
+	width:'100%'
 });
 
 win2.add(label2);
+
 //tab 3
 var win3 = Titanium.UI.createWindow({  
     title:'Saturday, April 18th',
@@ -107,7 +114,82 @@ var label3 = Titanium.UI.createLabel({
 	width:'auto'
 });
 
+var button6 = Titanium.UI.createButton({
+	title:'Brunch featuring University of Idaho Jazz Choir',
+	top:'40%',
+	left:'20%'
+	});
+
+var button7 = Titanium.UI.createButton({
+	top:'40%',
+	left:'60%',
+	title:'Key for a Cure Fun Run',
+});
+
+var button8 = Titanium.UI.createButton({
+	top:'50%',
+	left:'20%',
+	title:'Official UI Class Ring Sales',
+	
+});
+	var button9 = Titanium.UI.createButton({
+	top:'50%',
+	left:'60%',
+	title:'Annual Phi Delta Theta Turtle Derby',
+});
+
+var button10 = Titanium.UI.createButton({
+	top:'60%',
+	left:'20%',
+	title:'Greenthumb 101',
+});
+
+var button11 = Titanium.UI.createButton({
+	top:'60%',
+	left:'60%',
+	title:'Greenthumb 201',
+});
+
+var button12 = Titanium.UI.createButton({
+	top:'70%',
+	left:'20%',
+	title:'Dinner and Entertainment with FRANGELA',
+});
+
+button6.addEventListener('click', function(e) {alert
+	('Brunch featuring University of Idaho Jazz Choir\nPitman Center Ballroom\nApril 18th, 9:30 a.m.\nCost: $20/person');
+});
+
+button7.addEventListener('click', function(e) {alert
+	('Key for a Cure Fun Run\nStart at Memorial Gym\nApril 18th, 10:00 a.m.\nCost: $17 including T-shirt; $10 without');
+});
+
+button9.addEventListener('click', function(e) {alert
+	('Official UI Class Ring Sales\nVandalStore Plaza, 710 Deakin Ave.\nApril 18th, 10:00 a.m.-3:00 p.m.');
+});
+button10.addEventListener('click', function(e) {alert
+	('Greenthumb 101\n6th Street Greenhouse.\nApril 18th, 10:30 a.m.-11:30 a.m.\nCost:$15 per pot/basket | Pay at the door');
+});
+
+button11.addEventListener('click', function(e) {alert
+	('Annual Phi Delta Theta Turtle Derby\n804 Elm St.\nApril 18th, 11:00 a.m.\nCost: Free');
+});
+
+button12.addEventListener('click', function(e) {alert
+	('Greenthumb 201\n6th Street Greenhouse\nApril 18th, 1:00 p.m. - 2:00 p.m.\nCost: $15 per pot/basket | Pay at the door');
+});
+
+
 win3.add(label3);
+label3.add(button6);
+label3.add(button7);
+label3.add(button9);
+label3.add(button10);
+label3.add(button11);
+label3.add(button12);
+
+
+
 //
 //  add tabs
 //
@@ -117,3 +199,6 @@ tabGroup.addTab(tab3);
 
 // open tab group
 tabGroup.open();
+
+
+
