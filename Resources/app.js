aa2e72f399a54cfed9c577171ca454f5c3704ee0
Kyzjin <jparish@vandals.uidaho.edu>
2015-04-08 13:12:50 -0700

    Update app.js

--- Resources/app.js
+++ Resources/app.js
@@ -1,125 +1,119 @@
-var win1 = Titanium.UI.createWindow({
-	backgroundImage:'\andriod\banner.jpg'
+// this sets the background color of the master UIView (when there are no windows/tab groups on it)
+Titanium.UI.setBackgroundColor('#000');
+
+// create tab group
+var tabGroup = Titanium.UI.createTabGroup();
+
+
+//
+// create base UI tab and root window
+//
+var win1 = Titanium.UI.createWindow({  
+    title:'Friday, April 17th',
+    backgroundImage:"pictures/banner.jpg",
+    scrollable:true
+});
+var tab1 = Titanium.UI.createTab({  
+    icon:'KS_nav_ui.png',
+    title:'Friday Events',
+    window:win1
+});
+var label1 = Titanium.UI.createLabel({
+	color:'black',
+	text:'April 17th-18th',
+	font:{fontSize:60,fontFamily:'Helvetica Neue'},
+	textAlign:'center',
+	width:'auto',
+	layout:'vertical'
 });
 
-var button1 = Titanium.UI.createButton({
-	title:'Event Schedule',
-	top:'90%',
-	alignment:'center'
+var ring = Titanium.UI.createButton({
+	title:'Official UI Class Ring Sales',
+	top: 100
+});
+ring.addEventListener('click', function(e) {
+	alert('Time - 1pm to 5pm\nPlace - VandaStore Plaza');
+	});
+
+var race = Titanium.UI.createButton({
+	title:'Amazing Race Traditions Challenge',
+	top: 115
+});
+race.addEventListener('click', function(e) {
+	alert('Time - 5pm to 6pm\nPlace - Meet at VandalStore\nCost - $10, includes Tradition Keepers book');
 });
 
-button1.addEventListener('click', function(){
-	var win2 = Titanium.UI.createWindow({
-		backgroundColor:'#999'
-	});
-	var table1 = Titanium.UI.createTableView({
-		backgroundColor:'#999',
-		scrollable: true
-	});
-	var button2 = Titanium.UI.createButton({
-		backgroundImage:'\android\Flower1.png',
-		text:'Official UI Class Ring Sale',
-	});
-	var button3 = Titanium.UI.createButton({
-		backgroundImage:'\android\Flower2.png',
-		text:'Amazing Race Traditions Challenge',
-	});
-	var button4 = Titanium.UI.createButton({
-		backgroundImage:'\android\Flower1.png',
-		text:'CALS Moms Weekend Gala Wine and Cheese Tasting',
-	});
-	var button5 = Titanium.UI.createButton({
-		backgroundImage:'\android\Flower2.png',
-		text:'Bella Notte: An Italian Dinner',
-	});
-	button2.addEventListener('click', function(){
-		var win3 = Titanium.UI.createWindow({
-			backgroundColor:'#999',
-			text:'Official UI Class Ring Sales',
-			text:'VandalStore Plaza',
-			text:'April 17th, 1-5 p.m.',
-			textAlign:'center',
-		});
-		var button6 = Titanium.UI.createButton({
-			top:'90%',
-			left:'80%',
-			text:'Back'
-		});
-		win3.add(button6);
-		button6.addEventListener('click', function() {
-			win3.close();
-		});
-		win3.open();
-	});
-	button3.addEventListener('click', function(){
-		var win4 = Titanium.UI.createWindow({
-			backgroundColor:'#999',
-			text:'Amazing Race Traditions Challenge',
-			text:'VandalStore',
-			text:'April 17th, 5-6 p.m.',
-			text:'Cost $10, includes Tradition Keepers book',
-			textAlign:'center'
-		});
-		var button7 = Titanium.UI.createButton({
-			top:'90%',
-			left:'80%',
-			text:'Back'
-		});
-		win4.add(button7);
-		button7.addEventListener('click', function() {
-			win4.close();
-		});
-		win4.open();
-	)};
-	button4.addEventListener('click', function(){
-		var win5 = Titanium.UI.createWindow({
-			backgroundColor:'#999',
-			text:'CALS Moms Weekend Gala Wine and Cheese Tasting',
-			text:'Pitman Center International Ballroom',
-			text:'April 17th, 5-8 p.m.',
-			text:'Cost: $20/person or $35/couple',
-			text:'RSVP required',
-			textAlign:'center'
-		});
-		var button8 = Titanium.UI.createButton({
-			top:'90%',
-			left:'80%',
-			text:'Back'
-		});
-		win5.add(button8);
-		button8.addEventListener('click', function() {
-			win5.close();
-		});
-		win5.open();
-	)};
-	button5.addEventListener('click', function(){
-		var win6 = Titanium.UI.createWindow({
-			backgroundColor:'#999',
-			text:'Bella Notte: An Italian Dinner',
-			text:'St. Augustines Catholic Center',
-			text:'April 17th, 5-8:30 p.m.',
-			text:'Cost: $6/person, $5/student, $18/group of 4',
-			textAlign:'center'
-		});
-		var button9 = Titanium.UI.createButton({
-			top:'90%',
-			left:'80%',
-			text:'Back'
-		});
-		win6.add(button9);
-		button9.addEventListener('click', function() {
-			win6.close();
-		});
-		win6.open({);
-	});
-	table1.add(button2);
-	table1.add(button3);
-	table1.add(button4);
-	table1.add(button5);
-	win2.add(table1);
-	win2.open();
+var gala = Titanium.UI.createButton({
+	title:'CALS Moms Weekend Gala Wine and Cheese Tasting',
+	top: 130
+});
+gala.addEventListener('click', function(e) {
+	alert('Time - 5pm to 8pm\nPlace - Pitman Center International Ballroom\nCost - $20/person or $35/couple\nRSVP Required');
 });
 
-win1.add(button1);
+var bella = Titanium.UI.createButton({
+	title:'Bella Notte: An Italian Dinner',
+	top: 145
+});
+bella.addEventListener('click', function(e) {
+	alert('Time - 5pm to 8:30pm\nPlace - St. Augustines Catholic Center\nCost - $5/student, $6/person, $18/group of 4');
+});
 
-win1.open();
+label1.add(ring);
+label1.add(race);
+label1.add(gala);
+label1.add(bella);
+
+win1.add(label1);
+
+//
+// create controls tab and root window
+//
+var win2 = Titanium.UI.createWindow({  
+    title:'Contact Information',
+    backgroundImage:'pictures/banner.jpg',
+    layout:'vertical',
+});
+var tab2 = Titanium.UI.createTab({  
+    icon:'KS_nav_ui.png',
+    title:'Contact',
+    window:win2
+});
+
+var label2 = Titanium.UI.createLabel({
+	color:'black',
+	text:'Welcome Moms',
+	font:{fontSize:35,fontFamily:'Helvetica Neue'},
+	textAlign:'top',
+	width:'auto'
+});
+
+win2.add(label2);
+//tab 3
+var win3 = Titanium.UI.createWindow({  
+    title:'Saturday, April 18th',
+    backgroundImage:"pictures/banner.jpg"
+});
+var tab3 = Titanium.UI.createTab({  
+    icon:'KS_nav_ui.png',
+    title:'Saturday Events',
+    window:win3
+});
+var label3 = Titanium.UI.createLabel({
+	color:'black',
+	text:'April 17th-18th',
+	font:{fontSize:60,fontFamily:'Helvetica Neue'},
+	textAlign:'center',
+	width:'auto'
+});
+
+win3.add(label3);
+//
+//  add tabs
+//
+tabGroup.addTab(tab1);  
+tabGroup.addTab(tab2);  
+tabGroup.addTab(tab3);
+
+// open tab group
+tabGroup.open();
